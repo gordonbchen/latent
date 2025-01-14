c9239d89519031e8ffc5f778071f48972dd4637b
gordonbchen <gordonbchen@gmail.com>
2025-01-14 02:48:11 -0500

    add wgan script (wip)

--- latent/wgan.py
+++ latent/wgan.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 
 import torch
+import torch.linalg as LA
 from cli_params import CLIParams
 from torch import nn
 from torch.optim import Adam, Optimizer
@@ -17,12 +18,18 @@ class HyperParams(CLIParams):
     latent_dim: int = 128
     relu_leak: float = 0.2
     conv_filters: int = 128
+    grad_penalty_coeff: float = 10.0
 
     lr: float = 1e-4
     batch_size: int = 128
-    train_epochs: int = 64
+    train_steps: int = 16_000
+    critic_steps: int = 5
+    log_steps: int = 500
 
-    output_dir: str = "outputs/gan/test"
+    momentum_beta_1: float = 0.0
+    momentum_beta_2: float = 0.9
+
+    output_dir: str = "outputs/gan/wgan/test"
 
 
 class ConvBlock(nn.Module):
@@ -39,149 +46,156 @@ class ConvBlock(nn.Module):
         super().__init__()
 
         conv = nn.ConvTranspose2d if transposed else nn.Conv2d
-        self.conv = conv(in_channels, out_channels, kernel_size, stride, padding, bias=False)
-
-        self.bn = nn.BatchNorm2d(out_channels)
+        self.conv = conv(in_channels, out_channels, kernel_size, stride, padding, bias=True)
         self.act = nn.LeakyReLU(relu_leak) if relu_leak is not None else nn.ReLU()
 
     def forward(self, xb: torch.Tensor) -> torch.Tensor:
-        return self.act(self.bn(self.conv(xb)))
+        return self.act(self.conv(xb))
 
 
-class GAN(nn.Module):
-    def __init__(
-        self, latent_dim: int, data_channels: int, conv_filters: int, relu_leak: float
-    ) -> None:
-        super().__init__()
-        self.latent_dim = latent_dim
+def weight_init(module: nn.Module) -> None:
+    name = module.__class__.__name__
+    if name in ("Conv2d", "Conv2dTranspose"):
+        nn.init.kaiming_normal_(module.weight.data, mode="fan_in")
 
-        self.generator = nn.Sequential(
-            # (latent_dim, 1, 1).
-            ConvBlock(
-                latent_dim,
-                conv_filters * 4,
-                kernel_size=4,
-                stride=1,
-                padding=0,
-                transposed=True,
-            ),
-            # (conv_filters*4, 4, 4).
-            ConvBlock(conv_filters * 4, conv_filters * 2, transposed=True),
-            # (conv_filters*2, 8, 8).
-            ConvBlock(conv_filters * 2, conv_filters, transposed=True),
-            # (conv_filters, 16, 16).
-            nn.ConvTranspose2d(
-                conv_filters,
-                data_channels,
-                kernel_size=4,
-                stride=2,
-                padding=1,
-                bias=True,
-            ),
-            nn.Sigmoid(),
-            # (data_channels, 32, 32).
-        )
-        self.discriminator = nn.Sequential(
-            # (data_channels, 32, 32).
-            nn.Conv2d(data_channels, conv_filters, kernel_size=4, stride=2, padding=1, bias=True),
-            nn.LeakyReLU(relu_leak),
-            # (conv_filters, 16, 16).
-            ConvBlock(conv_filters, conv_filters * 2, relu_leak=relu_leak),
-            # (conv_filters*2, 8, 8).
-            ConvBlock(conv_filters * 2, conv_filters * 4, relu_leak=relu_leak),
-            # (conv_filters*4, 4, 4).
-            nn.Conv2d(conv_filters * 4, 1, kernel_size=4, stride=1, padding=0, bias=True),
-            # (1, 1, 1).
-            nn.Flatten(),
-            nn.Sigmoid(),
-        )
 
-        self.apply(self.weight_init)
+def Critic(HP: HyperParams, data_channels: int) -> nn.Module:
+    critic = nn.Sequential(
+        # (data_channels, 32, 32).
+        ConvBlock(data_channels, HP.conv_filters, relu_leak=HP.relu_leak),
+        # (conv_filters, 16, 16).
+        ConvBlock(HP.conv_filters, HP.conv_filters * 2, relu_leak=HP.relu_leak),
+        # (conv_filters*2, 8, 8).
+        ConvBlock(HP.conv_filters * 2, HP.conv_filters * 4, relu_leak=HP.relu_leak),
+        # (conv_filters*4, 4, 4).
+        nn.Flatten(),
+        # (conv_filters*4*4*4,).
+        nn.Linear(HP.conv_filters * 4 * 4 * 4, 1),
+    )
+    critic.apply(weight_init)
+    return critic
 
-    def optim_step(
-        self, xb: torch.Tensor, discriminator_optim: Optimizer, generator_optim: Optimizer
-    ) -> tuple[float, float, float, float, torch.Tensor]:
-        # Generate new images.
-        z = torch.rand((xb.shape[0], self.latent_dim, 1, 1), dtype=torch.float32, device="cuda")
-        generated = self.generator(z)
 
-        # Optimize discriminator.
-        discriminator_optim.zero_grad()
+def Generator(HP: HyperParams, data_channels: int) -> nn.Module:
+    generator = nn.Sequential(
+        # (latent_dim, 1, 1).
+        ConvBlock(
+            HP.latent_dim,
+            HP.conv_filters * 4,
+            kernel_size=4,
+            stride=1,
+            padding=0,
+            transposed=True,
+        ),
+        # (conv_filters*4, 4, 4).
+        ConvBlock(HP.conv_filters * 4, HP.conv_filters * 2, transposed=True),
+        # (conv_filters*2, 8, 8).
+        ConvBlock(HP.conv_filters * 2, HP.conv_filters, transposed=True),
+        # (conv_filters, 16, 16).
+        nn.ConvTranspose2d(
+            HP.conv_filters,
+            data_channels,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+            bias=True,
+        ),
+        nn.Sigmoid(),
+        # (data_channels, 32, 32).
+    )
+    generator.apply(weight_init)
+    return generator
 
-        real_probs = self.discriminator(xb)
-        generated_probs = self.discriminator(generated.detach())
 
-        eps = torch.finfo(torch.float32).eps
-        discriminator_loss = (
-            -torch.log(real_probs + eps).mean() - torch.log(1.0 - generated_probs + eps).mean()
-        )
+def optim_step(
+    xb: torch.Tensor,
+    critic: nn.Module,
+    generator: nn.Module,
+    critic_optim: Optimizer,
+    generator_optim: Optimizer,
+    step: int,
+    HP: HyperParams,
+) -> None:
+    # Generate new images.
+    z = torch.rand((HP.batch_size, HP.latent_dim, 1, 1), dtype=torch.float32, device="cuda")
+    fake_xb = generator(z)
 
-        discriminator_loss.backward()
-        discriminator_optim.step()
+    # Optimize critic.
+    critic_optim.zero_grad()
 
-        # Optimize generator.
+    real_loss = -critic(xb).mean()
+    fake_loss = critic(fake_xb.detach()).mean()
+
+    alpha = torch.rand((HP.batch_size, 1, 1, 1), dtype=torch.float32, device="cuda")
+    interps = (alpha * xb) + ((1 - alpha) * fake_xb.detach())
+    interps.requires_grad = True
+    jacobian = torch.autograd.grad(
+        outputs=critic(interps).sum(), inputs=interps, retain_graph=True
+    )[0]
+    grad_norm = LA.vector_norm(jacobian, dim=(1, 2, 3)).mean()  # TODO: flatten then norm.
+    grad_penalty = HP.grad_penalty_coeff * ((grad_norm - 1.0) ** 2.0)
+
+    critic_loss = real_loss + fake_loss + grad_penalty
+    critic_loss.backward()
+    critic_optim.step()
+
+    # Optimize generator.
+    is_last_step = step == (HP.train_steps - 1)
+    if ((step % HP.critic_steps) == 0) or is_last_step:
         generator_optim.zero_grad()
-
-        generated_probs = self.discriminator(generated)  # Recalc b/c discriminator was optimized.
-        generator_loss = -torch.log(generated_probs + eps).mean()
-
+        generator_loss = -1.0 * critic(fake_xb).mean()
         generator_loss.backward()
         generator_optim.step()
 
-        return (
-            discriminator_loss.item(),
-            generator_loss.item(),
-            real_probs.mean().item(),
-            generated_probs.mean().item(),
-            generated.detach().to("cpu"),
-        )
-
-    def weight_init(self, module: nn.Module) -> None:
-        name = module.__class__.__name__
-        if name in ("Conv2d", "Conv2dTranspose"):
-            nn.init.normal_(module.weight.data, 0.0, 0.02)
-        elif "BatchNorm" in name:
-            nn.init.normal_(module.weight.data, 1.0, 0.02)
-            nn.init.zeros_(module.bias.data)
+    # Log metrics.
+    if ((step % HP.log_steps) == 0) or is_last_step:
+        total_loss = critic_loss.item() + generator_loss.item()
+        loss_vals = {
+            "total": total_loss,
+            "critic": critic_loss.item(),
+            "generator": generator_loss.item(),
+        }
+        logger.add_scalars("loss", loss_vals, step)
+        logger.add_scalar("grad_norm", grad_norm.item(), step)
+        logger.add_images("generated", fake_xb.detach().to("cpu")[:24], step)
+        print(f"Step {step}: loss={total_loss:.6f}")
 
 
 if __name__ == "__main__":
     HP = HyperParams()
+    assert (HP.train_steps % HP.critic_steps) == 0
 
     transform = v2.Compose([v2.PILToTensor(), v2.ToDtype(torch.float32, scale=True), v2.Resize(32)])
     train_ds = MNIST("data", train=True, download=True, transform=transform)
     train_dl = DataLoader(
-        train_ds, batch_size=HP.batch_size, shuffle=True, num_workers=2, pin_memory=True
+        train_ds,
+        batch_size=HP.batch_size,
+        shuffle=True,
+        num_workers=2,
+        pin_memory=True,
+        drop_last=True,
     )
 
-    gan = GAN(HP.latent_dim, train_ds[0][0].shape[0], HP.conv_filters, HP.relu_leak).to("cuda")
-    discriminator_optim = Adam(gan.discriminator.parameters(), lr=HP.lr, betas=(0.5, 0.999))
-    generator_optim = Adam(gan.generator.parameters(), lr=HP.lr, betas=(0.5, 0.999))
+    data_channels = train_ds[0][0].shape[0]
+    critic = Critic(HP, data_channels).to("cuda").train()
+    generator = Generator(HP, data_channels).to("cuda").train()
+
+    betas = (HP.momentum_beta_1, HP.momentum_beta_2)
+    critic_optim = Adam(critic.parameters(), lr=HP.lr, betas=betas)
+    generator_optim = Adam(generator.parameters(), lr=HP.lr, betas=betas)
 
     logger = SummaryWriter(HP.output_dir)
 
-    for epoch in range(HP.train_epochs):
-        for xb, _ in train_dl:
-            xb = xb.to("cuda")
+    dl_iter = iter(train_dl)
+    for step in range(HP.train_steps):
+        try:
+            xb, _ = next(dl_iter)
+        except StopIteration:
+            dl_iter = iter(train_dl)
+            xb, _ = next(dl_iter)
+        xb = xb.to("cuda")
 
-            discriminator_loss, generator_loss, mean_real_prob, mean_generated_prob, generated = (
-                gan.optim_step(xb, discriminator_optim, generator_optim)
-            )
+        optim_step(xb, critic, generator, critic_optim, generator_optim, step, HP)
 
-        total_loss = discriminator_loss + generator_loss
-        loss_vals = {
-            "total": total_loss,
-            "discriminator": discriminator_loss,
-            "generator": generator_loss,
-        }
-        logger.add_scalars("loss", loss_vals, epoch)
-
-        logger.add_scalars(
-            "probs", {"real": mean_real_prob, "generated": mean_generated_prob}, epoch
-        )
-
-        logger.add_images("generated", generated[:24], epoch)
-
-        print(f"Epoch {epoch}: loss={total_loss:.6f}")
-
+    del dl_iter  # HACK: Error with infinite data loader and threads.
     logger.close()
